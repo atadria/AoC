2bc76746660d78e5f2732e5068cf7124e439def3
adri <tokarska.adrianna@gmail.com>
2021-12-01 22:01:50 +0100

    add template for next puzzles

--- day01/main.py
+++ day01/main.py
@@ -1,11 +1,9 @@
+# https://adventofcode.com/2021/day/1
+
 INPUT_PATH = 'input.txt'
 
-if __name__ == '__main__':
-    data = []
-    for line in open(INPUT_PATH, 'r', encoding='UTF-8'):
-        data.append(int(line))
 
-    # part 1
+def part1(data):
     increase = 0
     last_measurement = None
     for m in data:
@@ -14,12 +12,24 @@ if __name__ == '__main__':
         last_measurement = m
     print('part 1: ', increase)
 
-    # part 2
+
+def part2(data):
     increase = 0
     last_sum = None
-    for i in range(len(data)-2):
-        current_sum = sum(data[i:i+3])
+    for i in range(len(data) - 2):
+        current_sum = sum(data[i:i + 3])
         if last_sum and current_sum > last_sum:
             increase += 1
         last_sum = current_sum
     print('part 2: ', increase)
+
+
+if __name__ == '__main__':
+    input_data = []
+    for line in open(INPUT_PATH, 'r', encoding='UTF-8'):
+        input_data.append(int(line))
+
+    part1(input_data)
+    part2(input_data)
+
+
---
+++ day02/main.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2021/day/2
+
+INPUT_PATH = 'input.txt'
+
+
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
+
+
+if __name__ == '__main__':
+    input_data = []
+    for line in open(INPUT_PATH, 'r', encoding='UTF-8'):
+        input_data.append(int(line))
+
+    part1(input_data)
+    part2(input_data)
