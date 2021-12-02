2cd3fe3cea8f4698cdcf3e48d625c62a95a07c28
adri <tokarska.adrianna@gmail.com>
2021-12-02 08:15:19 +0100

    day 2

--- day02/main.py
+++ day02/main.py
@@ -4,17 +4,45 @@ INPUT_PATH = 'input.txt'
 
 
 def part1(data):
-    pass
+    h = 0
+    v = 0
+    for c, p in data:
+        if c == 'up':
+            v -= p
+        elif c == 'down':
+            v += p
+        else:
+            h += p
+    print(v * h)
 
 
 def part2(data):
-    pass
+    """
+    * down X increases your aim by X units.
+    * up X decreases your aim by X units.
+    * forward X does two things:
+        - It increases your horizontal position by X units.
+        - It increases your depth by your aim multiplied by X.
+    """
+    aim = 0
+    h = 0
+    d = 0
+    for c, p in data:
+        if c == 'up':
+            aim -= p
+        elif c == 'down':
+            aim += p
+        else:
+            h += p
+            d += aim * p
+    print(d * h)
 
 
 if __name__ == '__main__':
     input_data = []
-    for line in open(INPUT_PATH, 'r', encoding='UTF-8'):
-        input_data.append(int(line))
+    for line in open(INPUT_PATH, 'r'):
+        c, p = line.strip().split()
+        input_data.append((c.strip(), int(p)))
 
     part1(input_data)
     part2(input_data)
